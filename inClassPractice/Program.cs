f944ddc6b8bc08a02fb38dbc1343cc418d02d958
John Cunningham <jc468040@wtstudents.wtamu.edu>
2018-09-17 11:09:09 -0500

    initial commit

---
+++ inClassPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace inClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

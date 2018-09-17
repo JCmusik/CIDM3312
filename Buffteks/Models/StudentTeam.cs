1be55a4d56733fb57c93d9e095daa2f3eacb7cad
John Cunningham <jc468040@wtstudents.wtamu.edu>
2018-09-17 12:55:29 -0500

    added studentteam model

---
+++ Buffteks/Models/StudentTeam.cs
@@ -0,0 +1,10 @@
+namespace inClassPractice.Models
+{
+    public class StudentTeam
+    {
+        public int StudentId { get; set; }
+        public int TeamId { get; set; }
+        public Team Team { get; set; }
+        public Student Student { get; set; }
+    }
+}

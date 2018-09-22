a07ac115eb49e038a760cf0d5a11b4f3105e8ccb
johncunningham <jcmusik@gmail.com>
2018-09-22 11:58:01 -0500

    updated ToString Layout

--- Buffteks/Program.cs
+++ Buffteks/Program.cs
@@ -8,32 +8,32 @@ namespace Buffteks
         static void Main(string[] args)
         {
             // Create new record
-            System.Console.Write("What is your first name: ");
-            var fName = Console.ReadLine();
+            // System.Console.Write("What is your first name: ");
+            // var fName = Console.ReadLine();
 
-            System.Console.Write("What is your last name: ");
-            var lName = Console.ReadLine();
+            // System.Console.Write("What is your last name: ");
+            // var lName = Console.ReadLine();
 
-            System.Console.Write("What is your email: ");
-            var email = Console.ReadLine();
+            // System.Console.Write("What is your email: ");
+            // var email = Console.ReadLine();
 
-            System.Console.WriteLine("What is your phone number: ");
-            var phoneNum = Console.ReadLine();
+            // System.Console.WriteLine("What is your phone number: ");
+            // var phoneNum = Console.ReadLine();
 
-            using (var context = new AppDbContext())
-            {
-                context.Student.Add(new Student {FirstName = fName, LastName = lName, Email = email, PhoneNumber = phoneNum});
-                context.SaveChanges();
-            }
+            // using (var context = new AppDbContext())
+            // {
+            //     context.Student.Add(new Student {FirstName = fName, LastName = lName, Email = email, PhoneNumber = phoneNum});
+            //     context.SaveChanges();
+            // }
 
             // Read from Database
-            // using(var context = new AppDbContext())
-            // {
-            //    foreach (var s in context.Student)
-            //    {
-            //        System.Console.WriteLine(s);
-            //    }
-            // }
+            using(var context = new AppDbContext())
+            {
+               foreach (var s in context.Student)
+               {
+                   System.Console.WriteLine($"\n{s}");
+               }
+            }
 
             // Update new record
 

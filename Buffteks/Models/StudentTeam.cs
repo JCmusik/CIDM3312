c5601374c73253d42482cc660154ad07118ea9d1
johncunningham <jcmusik@gmail.com>
2018-09-22 21:10:34 -0500

    added XML class comment

--- Buffteks/Models/StudentTeam.cs
+++ Buffteks/Models/StudentTeam.cs
@@ -1,8 +1,9 @@
 namespace Buffteks.Models
 {
     /// <summary>
-    /// Extra entity for many to many relationship of Student and Team  Fluent API's found in <cref="AppDbContext.cs"> in the <cref="OnModelCreating"> model builder
+    /// Extra entity for many to many relationship of Student and Team 
     /// </summary>
+    /// <reference>Fluent API's found in <cref="AppDbContext.cs"> in the <cref="OnModelCreating"> model builder</reference>
     public class StudentTeam
     {
         public int StudentId { get; set; }

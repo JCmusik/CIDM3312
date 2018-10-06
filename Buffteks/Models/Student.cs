29616248cb91124b3ad98cef5bf8e9a07b58ca0e
johncunningham <jcmusik@gmail.com>
2018-10-06 11:18:01 -0500

    updated Student model

--- Buffteks/Models/Student.cs
+++ Buffteks/Models/Student.cs
@@ -3,5 +3,7 @@ namespace Buffteks.Models
     public class Student : Person
     {
         public int StudentID { get; set; }
+        public int AdvisorID { get; set; }
+        public Advisor Advisor { get; set; }
     }
 }

97f822491c6bc99b358b8aaed9839f5993c028bc
johncunningham <jcmusik@gmail.com>
2018-10-07 22:13:56 -0500

    Added ID to the ToString() method

--- Buffteks/Models/Organization.cs
+++ Buffteks/Models/Organization.cs
@@ -7,12 +7,11 @@ namespace Buffteks.Models
             public int OrganizationID { get; set; }
             public string Name { get; set; }
 
-
         #endregion
 
         #region Methods
             
-            public override string ToString() => $"Organization: {this.Name}\nEmail: {this.Email}\nPhone Number: {this.PhoneNumber}";
+            public override string ToString() => $"ID: {this.OrganizationID} Organization: {this.Name}\nEmail: {this.Email}\nPhone Number: {this.PhoneNumber}";
 
         #endregion
     }

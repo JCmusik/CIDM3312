5d005303c73ed657a0bbc96ad44e2a86b2cd5eb8
JCmusik <jcmusik@gmail.com>
2018-10-10 11:58:59 -0500

    added db context class

--- LINQPractice/Models/AppDbContext.cs
+++ LINQPractice/Models/AppDbContext.cs
@@ -4,6 +4,12 @@ namespace LINQPractice.Models
 {
     public class AppDbContext : DbContext
     {
+        private const string connectionString = @"Data Source=LINQ.db";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString);
+        }
+
         
     }
 }

38a786631ff0335522074e8508220f13d885d18b
JCmusik <jcmusik@gmail.com>
2018-10-10 12:54:25 -0500

    updated ToString method

--- LINQPractice/Models/Student.cs
+++ LINQPractice/Models/Student.cs
@@ -2,11 +2,16 @@ namespace LINQPractice.Models
 {
     public class Student
     {
-        
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Rank { get; set; }
+        public byte TableNumber { get; set; }   
 
 
         #region Methods
-            public override string ToString() => $"";
+            public override string ToString() => $"{this.FirstName} {this.LastName}\n{this.Email}\nClassification: {this.Rank}\nTable: {this.TableNumber}\n";
         #endregion
     }
 }

77e81de3085a5dd0664b4d1ab272ee1c29245a3e
John Cunningham <John.Cunningham@csi.corp>
2018-11-01 09:15:12 -0500

    edited book title

--- MidTerm/DataAccess/SeedDatabase.cs
+++ MidTerm/DataAccess/SeedDatabase.cs
@@ -11,7 +11,6 @@ namespace DataAccess
         {
             using (var db = new AppDbContext())
             {
-                db.Database.EnsureDeleted();
                 db.Database.EnsureCreated();
 
                 var adamFreeman = new Author

c04f6b69b37696816b223954575b28a4e0298dc5
JCmusik <jcmusik@gmail.com>
2018-11-24 23:38:43 -0600

    more setup for project

--- FinalProject/BufftekModels/SeedDatabase.cs
+++ FinalProject/BufftekModels/SeedDatabase.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using DataAccess;
 
@@ -16,13 +17,90 @@ namespace BufftekModels
                     return;   // DB members table been seeded
                 }
 
-                // create add Members, save changes`
+                // create add Members, save changes
+                //Create at least ten Members
+                var members = new List<Member>
+                {
+                    new Member
+                    {
+
+                    },
+                    new Member
+                    {
+
+                    },
+                    new Member
+                    {
+
+                    },
+                    new Member
+                    {
+
+                    },
+                    new Member
+                    {
+
+                    },
+                    new Member
+                    {
+
+                    },
+                    new Member
+                    {
+
+                    },
+                    new Member
+                    {
+
+                    },
+                    new Member
+                    {
+
+                    },
+                    new Member
+                    {
+
+                    }
+                };
+                db.AddRange(members);
+                db.SaveChanges();
+
 
                 // create, add Clients, save changes
+                //Create at least two clients
+                var clients = new List<Client>
+                {
+                    new Client
+                    {
+
+                    },
+                    new Client
+                    {
+
+                    }
+                };
+
+                db.AddRange(clients);
+                db.SaveChanges();
 
                 // create, add Project, save changes
+                //Create at least two projects
+                var projects = new List<Project>
+                {
+                    new Project
+                    {
 
+                    },
+                    new Project
+                    {
 
+                    }
+                };
+
+                db.AddRange(projects);
+                db.SaveChanges();
+
+                //Assign Members and Clients to Projects
             }
         }
     }

2e487405f6c86f05c88acc8128e9433818109b48
JCmusik <jcmusik@gmail.com>
2018-11-25 19:41:33 -0600

    added OnModelCreating method for many-to-many relationships

--- FinalProject/BuffteksWebApp/Models/AppDbContext.cs
+++ FinalProject/BuffteksWebApp/Models/AppDbContext.cs
@@ -6,6 +6,12 @@ namespace BuffteksWebApp.Models
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MemberClient>()
+           .HasKey(mc => new { mc.MemberID, mc.ClientID });
+        }
+        public DbSet<MemberClient> MemberCLients { get; set; }
 
         public DbSet<Member> Members { get; set; }
         public DbSet<Client> Clients { get; set; }
